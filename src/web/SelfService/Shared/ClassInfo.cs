ae1b57e9003d0728ddaef6ce5fee73fbc5278553
sairamaj <sairamaj@gmail.com>
2020-07-02 19:25:09 -0700

    adding class attendance in progress

--- src/web/SelfService/Shared/ClassInfo.cs
+++ src/web/SelfService/Shared/ClassInfo.cs
@@ -5,7 +5,7 @@ namespace SelfService.Shared
     public class ClassInfo
     {
         public string Id { get; set; }
-        public DateTime DateTime { get; set; }
+        public DateTime DateTime {get; set;}
         public string ClassName { get; set; }
     }
 }
